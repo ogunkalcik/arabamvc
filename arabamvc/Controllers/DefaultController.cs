1cddeb30448414e720f7ccdc9956e8ad0103021b
evkur <evkur@EV>
2019-10-09 19:57:07 +0300

    Add project files.

---
+++ arabamvc/Controllers/DefaultController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace arabamvc.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listele()
+        {
+            return View();
+        }
+
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+
+        public ActionResult Sil()
+        {
+            return View();
+        }
+
+        public ActionResult Guncelle()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ arabamvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="arabamvc.MvcApplication" Language="C#" %>
